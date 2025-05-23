c2325e3053384f6a1705760dcbc27981289d0bbe
JhoanLondono <148402408+JhoanLondono@users.noreply.github.com>
2025-05-23 17:46:02 -0500

    Add files via upload
    Se agrego el punto 5 y parte del punto 6.

---
+++ ADA/Proyecto_final_ADA/Proyecto_final_ADA/Figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final_ADA
+{
+    public class Figura
+    {
+        public string Tipo { get; set; }
+        public double Area { get; set; }
+        public List<Punto> Puntos { get; set; }
+
+        public Figura(string tipo, double area, List<Punto> puntos)
+        {
+            Tipo = tipo;
+            Area = area;
+            Puntos = puntos;
+        }
+    }
+}
---
+++ ADA/Proyecto_final_ADA/Proyecto_final_ADA/Listapuntos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final_ADA
+{
+    public class Listapuntos
+    {
+        private List<Punto> puntos=new List<Punto>();
+        public void agregarPunto(Punto p) 
+        {
+            puntos.Add(p);
+        }
+
+        public List<Punto> obtenerPuntos()
+        { 
+            return puntos; 
+        }
+
+        public override string ToString()
+        {
+            string resultado = "Lista de puntos: \n";
+            foreach (Punto p in puntos)
+            { 
+                resultado += p.ToString(); 
+            }
+            return resultado;
+        }
+
+        public void Limpiar()
+        {
+            puntos.Clear();
+        }
+    }
+}
